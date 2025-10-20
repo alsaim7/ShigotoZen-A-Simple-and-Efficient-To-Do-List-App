1350f5d0a94312d1b1538fec98cf233c638b340d
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-10-21 00:27:28 +0530

    as

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Shigoto Zen',
-        short_name: 'ShigotoZen',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/favicon.png',
-            sizes: '64x64 32x32 24x24 16x16',
-            type: 'image/x-icon'
-          }
-        ]
-      }
-    })
   ],
 })
